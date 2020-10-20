1695a36c3b112fe1948c0a16f9a60ac3105ae315
jquinnie <jenn8866@gmail.com>
2020-10-20 14:56:54 -0600

    Rename reducer

--- src/reducers/reducerActiveBook.js
+++ src/reducers/reducerActiveBook.js
@@ -1,8 +1,10 @@
+import { BOOK_SELECTED } from '../actions/constants';
+
 // state argument is not app state, only the state this reducer is responsible for
 // when app first boots up, there is no state, so a default has to be in place
 export default function(state = null, action) {
   switch(action.type) {
-    case 'BOOK_SELECTED':
+    case BOOK_SELECTED:
       return action.payload;
   }
   return state;
