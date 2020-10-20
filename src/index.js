41890f2dfca2b5722940c8cc4afcc150a0225937
jquinnie <jenn8866@gmail.com>
2020-10-20 14:56:01 -0600

    Add middleware and some others, configure dotenv

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { BrowserRouter as Router } from 'react-router-dom';
+import ReduxPromise from 'redux-promise';
 
 import reducers from './reducers';
 import App from './components/app';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
